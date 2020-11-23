6bd4768b418e4a3d4fcb1db7203822a1656fd252
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-11-23 22:10:55 +0900

    HomeViewControllerにTableViewを貼り付け、その上にカスタムCellを表示させた

---
+++ CtaReactiveMaster/Sources/Home/ArticleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ArticleTableViewCell.swift
+//  CtaReactiveMaster
+//
+//  Created by 山根大生 on 2020/11/23.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- CtaReactiveMaster/Sources/Home/HomeViewController.swift
+++ CtaReactiveMaster/Sources/Home/HomeViewController.swift
@@ -9,7 +9,24 @@ import UIKit
 
 final class HomeViewController: UIViewController {
 
+    @IBOutlet weak var articleTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        articleTableView.delegate = self
+        articleTableView.dataSource = self
+        articleTableView.register(UINib(nibName: "ArticleTableViewCell", bundle: nil), forCellReuseIdentifier: "ArticleTableViewCell")
+    }
+}
+
+extension HomeViewController:UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as! ArticleTableViewCell
+        return cell
     }
 }
