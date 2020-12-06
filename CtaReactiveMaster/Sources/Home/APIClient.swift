c79344ad3344de5ea47c6383e0cf87bcc20f2950
yamanetaisei <swiftmantaisei@gmail.com>
2020-12-06 21:20:32 +0900

    APIClientファイルを追加,APIClientStructを定義

---
+++ CtaReactiveMaster/Sources/Home/APIClient.swift
@@ -0,0 +1,14 @@
+//
+//  APIClient.swift
+//  CtaReactiveMaster
+//
+//  Created by 山根大生 on 2020/12/03.
+//
+
+import Foundation
+
+struct APIClient {
+    func call() {
+        print("call!!!!!!!!")
+    }
+}
--- CtaReactiveMaster/Sources/Home/HomeViewController.swift
+++ CtaReactiveMaster/Sources/Home/HomeViewController.swift
@@ -18,10 +18,21 @@ final class HomeViewController: UIViewController {
         }
     }
     private var articles:[Article] = .init()
+    private let apiClient: APIClient
+    
+    init(apiClient:APIClient) {
+        self.apiClient = apiClient
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchArticles()
+        apiClient.call()
     }
 }
 
