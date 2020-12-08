af041f89b604f5daa2f520dae450bb387771c355
yamanetaisei <swiftmantaisei@gmail.com>
2020-12-09 00:23:34 +0900

    APIClientの処理をRXで書き変えた

--- CtaReactiveMaster/Sources/Home/APIClient.swift
+++ CtaReactiveMaster/Sources/Home/APIClient.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Alamofire
+import RxSwift
 
 struct APIClient {
     
@@ -16,21 +17,24 @@ struct APIClient {
         return decoder
     }()
     
-    func fetchArticles(completion: @escaping ([Article]) -> ()) {
-        let request = AF.request("http://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=30d06e4f9a934402a204fa89f9d9acfc")
-        request.response { response in
-            switch response.result {
-            case .success(let data):
-                guard let data = data else { return }
-                do {
-                    let decodeData = try decoder.decode(NewsDataModel.self, from: data)
-                    completion(decodeData.articles)
-                } catch {
+    func fetchArticles() -> Observable<[Article]> {
+        Observable<[Article]>.create { observer in
+            let request = AF.request("http://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=30d06e4f9a934402a204fa89f9d9acfc")
+            request.response { response in
+                switch response.result {
+                case .success(let data):
+                    guard let data = data else { return }
+                    do {
+                        let decodeData = try decoder.decode(NewsDataModel.self, from: data)
+                        observer.onNext(decodeData.articles)
+                    } catch {
+                        print(error)
+                    }
+                case .failure(let error):
                     print(error)
                 }
-            case .failure(let error):
-                print(error)
             }
+            return Disposables.create()
         }
     }
 }
