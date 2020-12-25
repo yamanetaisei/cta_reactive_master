52769b3b1d721d6e3f372a70da769e40232a31fe
yamanetaisei <swiftmantaisei@gmail.com>
2020-12-25 19:46:30 +0900

    いらない部分を削除

--- CtaReactiveMaster/Sources/Home/NewsArticleRepository.swift
+++ CtaReactiveMaster/Sources/Home/NewsArticleRepository.swift
@@ -7,36 +7,3 @@
 
 import Foundation
 
-protocol UrlGetter {
-    var url: String { get }
-}
-
-struct BitcoinNewsAPI: UrlGetter {
-    var url: String {
-        return "http://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=30d06e4f9a934402a204fa89f9d9acfc"
-    }
-}
-
-struct BusinessNewsAPI: UrlGetter {
-    var url: String {
-        return "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=30d06e4f9a934402a204fa89f9d9acfc"
-    }
-}
-
-struct AppleNewsAPI: UrlGetter {
-    var url: String {
-        return "http://newsapi.org/v2/everything?q=apple&sortBy=popularity&apiKey=30d06e4f9a934402a204fa89f9d9acfc"
-    }
-}
-
-struct TechCrunchNewsAPI: UrlGetter {
-    var url: String {
-        return "http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=30d06e4f9a934402a204fa89f9d9acfc"
-    }
-}
-
-struct WallStreetJournalNewsAPI: UrlGetter {
-    var url: String {
-        return "http://newsapi.org/v2/everything?domains=wsj.com&apiKey=30d06e4f9a934402a204fa89f9d9acfc"
-    }
-}
