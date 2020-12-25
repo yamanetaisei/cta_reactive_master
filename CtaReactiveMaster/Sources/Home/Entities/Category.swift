5584133e6943cf838de65a93888ad3549b0e087d
yamanetaisei <swiftmantaisei@gmail.com>
2020-12-26 00:08:12 +0900

    entitiesを追加

---
+++ CtaReactiveMaster/Sources/Home/Entities/Category.swift
@@ -0,0 +1,17 @@
+//
+//  Category.swift
+//  CtaReactiveMaster
+//
+//  Created by 山根大生 on 2020/12/25.
+//
+
+
+enum Category: String {
+    case business
+    case entertainment
+    case general
+    case health
+    case science
+    case sports
+    case technology
+}
